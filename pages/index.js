1999678cbba803592872b2e3b79ad8e673df8a4b
buccalon <buccalon@users.noreply.github.com>
2021-05-23 23:11:18 -0300

    Add Notion API client

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,31 @@
 import Head from "next/head";
 import { Box, Container, Text } from "@chakra-ui/react";
+import { getDatabase } from "../lib/notion";
 
-export default function Home() {
+export async function getServerSideProps() {
+  const data = await getDatabase();
+  return {
+    props: {
+      data,
+    },
+  };
+}
+
+export default function Home({ data }) {
+  console.log(data);
   return (
     <div>
       <Head>
         <title>Brunoland</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Box overflow="hidden" bg="purple.100" minH="100vh">
+      <Box overflow="hidden" bg="green.100" minH="100vh">
         <Container>
           <Text
-            color="pink.800"
+            color="cyan.900"
             fontWeight="semibold"
             mb="1rem"
             textAlign="center"
-            textDecoration="underline"
             fontSize={["4xl", "4xl", "5xl", "5xl"]}
           >
             Hello world!
