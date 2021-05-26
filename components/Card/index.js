6423d9092c12b4814880867d4adb1fdc507710bb
buccalon <buccalon@users.noreply.github.com>
2021-05-26 00:05:53 -0300

    Add component example

---
+++ components/Card/index.js
@@ -0,0 +1,67 @@
+import { Box, Image, Badge, StarIcon } from "@chakra-ui/react";
+
+export default function Card() {
+  const property = {
+    imageUrl: "https://bit.ly/2Z4KKcF",
+    imageAlt: "Rear view of modern home with pool",
+    beds: 3,
+    baths: 2,
+    title: "Airbnb example from Chakra UI",
+    formattedPrice: "$1,900.00",
+    reviewCount: 34,
+    rating: 4,
+  };
+
+  return (
+    <Box
+      maxW="lg"
+      borderWidth="1px"
+      borderRadius="lg"
+      overflow="hidden"
+      bgColor="white"
+    >
+      <Image src={property.imageUrl} alt={property.imageAlt} />
+
+      <Box p="6">
+        <Box d="flex" alignItems="baseline">
+          <Badge borderRadius="full" px="2" colorScheme="teal">
+            New
+          </Badge>
+          <Box
+            color="gray.500"
+            fontWeight="semibold"
+            letterSpacing="wide"
+            fontSize="xs"
+            textTransform="uppercase"
+            ml="2"
+          >
+            {property.beds} beds &bull; {property.baths} baths
+          </Box>
+        </Box>
+
+        <Box
+          mt="1"
+          fontWeight="semibold"
+          as="h4"
+          lineHeight="tight"
+          isTruncated
+        >
+          {property.title}
+        </Box>
+
+        <Box>
+          {property.formattedPrice}
+          <Box as="span" color="gray.600" fontSize="sm">
+            / wk
+          </Box>
+        </Box>
+
+        <Box d="flex" mt="2" alignItems="center">
+          <Box as="span" ml="2" color="gray.600" fontSize="sm">
+            {property.reviewCount} reviews
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,19 @@
 import React, { useState } from "react";
 import Head from "next/head";
+import Card from "../components/Card";
+import { getItemsByType } from "../utils/notion";
 import {
-  Spinner,
   Box,
   Container,
   Text,
   Tag,
   Flex,
-  ListItem,
   UnorderedList,
   Spacer,
 } from "@chakra-ui/react";
-import { getPosts } from "../lib/notion";
 
 export async function getServerSideProps() {
-  const data = await getPosts();
+  const data = await getItemsByType("post", "date");
   return {
     props: {
       data,
@@ -23,7 +22,8 @@ export async function getServerSideProps() {
 }
 
 export default function Home({ data }) {
-  const [posts, setPosts] = useState(data);
+  const [items, setItems] = useState(data);
+
   console.log(data);
   return (
     <div>
@@ -31,7 +31,7 @@ export default function Home({ data }) {
         <title>Brunoland</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Box overflow="hidden" bg="green.100" minH="200vh">
+      <Box overflow="hidden" bg="green.100" minH="100vh">
         <Container>
           <Text
             color="cyan.900"
@@ -40,21 +40,20 @@ export default function Home({ data }) {
             textAlign="center"
             fontSize={["4xl", "4xl", "5xl", "5xl"]}
           >
-            Hell
-            <Spinner />
-            &nbsp;World!
+            Hello World!
           </Text>
-
           <UnorderedList spacing={4}>
-            {posts.map((post) => (
+            {items.map((item) => (
               <Flex bg="cyan.900" h="60px" color="green.100" borderRadius="md">
-                <Box p="4">{post.properties.title.title[0].plain_text}</Box>
+                <Box p="4">{item.properties.title.title[0].plain_text}</Box>
                 <Spacer />
                 <Box p="4">
-                  <Tag>{post.properties.date.date.start}</Tag>
+                  <Tag>{item.properties.date.date.start}</Tag>
                 </Box>
               </Flex>
             ))}
+
+            <Card />
           </UnorderedList>
         </Container>
       </Box>
